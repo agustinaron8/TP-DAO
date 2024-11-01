69ac19d808e43c552999a4294e8a2501db379049
agustinaron8 <agustinaron8@gmail.com>
2024-11-01 10:44:26 -0300

    Separo clases en != archivos

---
+++ claseAutor.py
@@ -0,0 +1,6 @@
+class Autor(self):
+    def __init__(self, ID, nombre, apellido, nacionalidad):
+        self.ID = ID
+        self.nombre = nombre
+        self.apellido = apellido
+        self.nacionalidad = nacionalidad
---
+++ clasePrestamo.py
@@ -0,0 +1,7 @@
+class Prestamo(self):
+    def __init__(self, ID, usuario, libro, fechaPrestamo, fechaDevolucion):
+        self.ID = ID
+        self.usuario = usuario
+        self.libro = libro
+        self.fechaPrestamo = fechaPrestamo
+        self.fechaDevolucion = fechaDevolucion
